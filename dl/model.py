80276c4723c9b65a4c5c8542d1f2748df92234cd
Super-Louis <liuchao12119@126.com>
2018-11-06 15:04:13 +0800

    add dl

--- dl/model.py
+++ dl/model.py
@@ -11,13 +11,14 @@ from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense, Embedding
 from keras.layers import LSTM
+from keras.callbacks import ModelCheckpoint
 
 # load word2num dict
 with open('../data/word2num', 'r') as f:
     word2num = json.load(f)
 
-X = np.load('../data/X_lstm.npy')
-Y = np.load('../data/Y_lstm.npy')
+X = np.load('../data/X_lstm.npy')[:10000]
+Y = np.load('../data/Y_lstm.npy')[:10000]
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
 
@@ -37,17 +38,18 @@ model.add(Embedding(len(word2num),128))
 model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
 # 构建最后的全连接层, 最后输出维度为1
 model.add(Dense(1, activation='sigmoid'))
+cp = ModelCheckpoint('./models/model.{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss', verbose=0,
+                         save_best_only=False, save_weights_only=False, mode='auto', period=1)
 
 # compile
-
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
-model.fit(X_train, Y_train, batch_size=batch_size, epochs=15, validation_data=(X_test, Y_test))
+model.fit(X_train, Y_train, batch_size=batch_size, epochs=10, validation_data=(X_test, Y_test), callbacks=[cp])
 
 # evaluate
 score = model.evaluate(X_test, Y_test, batch_size=batch_size)
 print('Test loss:', score[0])
 print('Test accuracy', score[1])
-model.save('lstm_keras')
+model.save('lstm_keras2')
 
